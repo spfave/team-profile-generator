025fe5592f0f5ed1e7e6fae0fc4a51d2f8449dfc
Sebastian F <sebpfave2@gmail.com>
2021-04-25 00:15:04 -0400

    Complete and test index.js script

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // Import library modules
 const Team = require("./lib/team");
+const TeamSite = require("./lib/teamsite");
 
 // Script Execution
 const team = new Team();
 team
-  .collectTeamData()
-  .then((team) => console.log(team))
+  .collectTeamProps()
+  .then((team) => {
+    const teamSite = new TeamSite(team);
+    teamSite.generateTeamSite();
+  })
+  .then(() =>
+    console.log("Team site HTML page created and available in 'dist' folder")
+  )
   .catch((err) => console.log(err));
